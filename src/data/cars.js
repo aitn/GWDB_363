286f919ccd2c6adf05143423b72ffd00a6332ac2
Joel Norris <joel.r.norris@gmail.com>
2017-11-13 11:45:53 -0600

    added base project assets

---
+++ src/data/cars.js
@@ -0,0 +1,30 @@
+// src/data/cars.js
+
+const cars = [
+    {
+        'id':       '123456',
+        'make':     'Aucura',
+        'model':    'MDX',
+        'year':     '2003',
+        'color':    'silver',
+        'img':      'acura_mdx_2003.jpg',
+    },
+    {
+        'id': 'SvT11RaP',
+        'year': '2011'
+        'make': 'Ford',
+        'model': 'F150',
+        'trim': 'SVT Raptor',
+        'color': 'Ingot Silver',
+        'img': 'raptorious.png'
+    },
+    {
+        'id': '082017',
+        'make': 'Ford',
+        'model': 'Focus',
+        'year': '2013',
+        'color': 'black',
+        'img': 'ford-focus.jpg',
+    }
+];
+
